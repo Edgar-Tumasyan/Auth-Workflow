dd26711388274ead25a02c8b438105eb865b3ee9
Edgar-Tumasyan <tumasyan.edgar@mail.ru>
2022-09-20 15:27:41 +0400

    update register controller, create verificationToken with crypto

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
+const crypto = require('crypto');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,11 +16,24 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const user = await User.create({ name, email, password, role });
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  const verificationToken = crypto.randomBytes(40).toString('hex');
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    verificationToken,
+  });
+
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      msg: 'Success! Please check your email to verify account',
+      verificationToken: user.verificationToken,
+    });
 };
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -40,6 +54,7 @@ const login = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
+
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
     httpOnly: true,
