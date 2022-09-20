7bee392b0f770bdb7a3b1d0abf86bcb539ffc68e
Edgar-Tumasyan <tumasyan.edgar@mail.ru>
2022-09-20 16:07:41 +0400

    created verifyEmail function in authController and setup a '/verify-email' route in authRoutes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,6 +32,28 @@ const register = async (req, res) => {
   });
 };
 
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Verification failed');
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError('Verification failed');
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = '';
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Email verified' });
+};
+
 const login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -70,4 +92,5 @@ module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
