4186ed66afa1b9d3521b078da01e27201f3b8f74
Edgar-Tumasyan <tumasyan.edgar@mail.ru>
2022-09-21 20:43:06 +0400

    Updated login function (create refreshToken with crypto). Updated createJWT and attachCookiesToResponse functions (send two cookies-accessToken and refreshToken).

--- server/utils/nodemailerConfig.js
+++ server/utils/nodemailerConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   host: 'smtp.ethereal.email',
   port: 587,
   auth: {
-    user: 'joyce.olson16@ethereal.email',
-    pass: '14je4N5tRhE2JAxgaE',
+    user: 'neva.vandervort96@ethereal.email',
+    pass: 'Tk9spzUWj3TVywTePG',
   },
 };
